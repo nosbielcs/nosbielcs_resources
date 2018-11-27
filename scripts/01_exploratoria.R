8af77b43150a96ec67362b8380ac1e1b45f2ec59
Cleibson Almeida <nosbielcs@bol.com.br>
2018-11-27 01:36:52 -0200

    hel

--- scripts/01_exploratoria.R
+++ scripts/01_exploratoria.R
@@ -1,2 +1,28 @@
 #Análise
 
+#Carregando Pacotes
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(forcats)
+library(readr)
+library(tidyr)
+
+#Leitura dos Dados
+library(readr)
+dados <- read_delim("dados/opendata_aig_brazil.csv", 
+                  "~", 
+                  escape_double = FALSE, 
+                  trim_ws = TRUE)
+
+names(dados)
+head(dados)
+attach(dados)
+
+#Total de Acidentes
+ggplot(dados) + 
+  geom_bar(data = dados, mapping = aes(dados$ocorrencia_classificacao)) +
+  xlab("Classificação") +
+  ylab("Total")
+
+
